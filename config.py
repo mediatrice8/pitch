cd8e40d7af5c85b422cb1f12e1b5b24472b38491
mediatrice8 <kampiremediatrice@gmail.com>
2019-09-25 11:44:41 +0200

    configuration of all things will be happen in app

---
+++ config.py
@@ -0,0 +1,29 @@
+import os
+
+class Config:
+    
+    SECRET_KEY = 'anonymous'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://media:media12@localhost/pitch'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
+class ProdConfig(Config):
+    """Production configuration class that inherits from the main configurations class"""
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://media:media12@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+}
